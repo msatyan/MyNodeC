d056b5aadadb47c81d460537dd06f019bdcb075e
Satyan <msatyan@gmail.com>
2019-04-10 23:31:53 -0500

    Starting the promise work

---
+++ src/MyPromise1.cpp
@@ -0,0 +1,12 @@
+
+// TODO:
+// napi_create_promise()
+// napi_resolve_deferred()
+// napi_reject_deferred()
+// napi_create_async_work();
+// napi_queue_async_work();
+// napi_delete_async_work()
+
+// eg:
+// https://github.com/theapi/napi_async_promise
+// https://github.com/gongfan99/node_napi_promise_example
