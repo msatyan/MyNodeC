d775ebc4041cb122d131300290f9a1abbebce8d8
Satyan <msatyan@gmail.com>
2019-10-06 12:19:20 -0500

    solve the error while creating wraper obj

--- src/addon_main_4cpp.cpp
+++ src/addon_main_4cpp.cpp
@@ -5,8 +5,8 @@
 napi_value MyNodeAddonApiInitSession1( napi_env c_env, napi_value c_exports)
 {
   // Create a C++ wrapper objects to be used with node-addon-api
-  Napi::Env    env     = Napi::Env::Env(c_env);
-  Napi::Object exports = Napi::Object::Object	(	c_env, c_exports );
+  Napi::Env    env     = Napi::Env(c_env);
+  Napi::Object exports = Napi::Object(c_env, c_exports);
 
   exports.Set(Napi::String::New(env, "callEmit"), Napi::Function::New(env, CallEmit));
 
