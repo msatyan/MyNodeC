5200d8bc4d6eff6b141f92d43b7f69c52edde645
Satyan <msatyan@gmail.com>
2019-10-30 23:43:03 -0500

    obj format

--- test/TestExtensions.js
+++ test/TestExtensions.js
@@ -167,9 +167,9 @@ function TestPromise() {
 
 
 //////////////////////////////
-function TestNativeCalls2() {
+function PrintJsonObj() {
     console.log(' ');
-    console.log('///////////  TestNativeCalls2 //////////////');
+    console.log('////// Native function printing JSON object ////////');
 
     // Send some value to C function
     console.log();
@@ -179,7 +179,7 @@ function TestNativeCalls2() {
         myNum_1: 1257,
         myNum_2: 1257.28,
         myBool_1: true,
-        myArray_1: [2, 4, 6, 9, { a1: 10, b1 : 20.34, c1: 30}],
+        myArray_1: [2, 4, 6, 9, { a1: 10, b1 : 20.34, c1: 30, d1: [ 35,  2.6 ], e1: 45 } ],
         myNullVal_1: null,
         myObj_1: { a1: 1, b1 : 2},
         xyz: 'for more testing!'
@@ -188,6 +188,8 @@ function TestNativeCalls2() {
     let dbg_on = false;
     myaddon.CPrintJsonObject(val1, dbg_on);
 
+    console.log();
+    console.log('////// With Debug On /////' );
     dbg_on = true;
     myaddon.CPrintJsonObject(val1, dbg_on);
     // myaddon.CPrintJsonObject2(val1);
@@ -204,10 +206,8 @@ function Main() {
 
     TestPromise();
 
-    // TestNativeCalls2();
+    PrintJsonObj();
 }
 
-// Main();
+Main();
 
-
-TestNativeCalls2();
